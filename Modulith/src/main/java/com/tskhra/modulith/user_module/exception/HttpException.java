e8bf1e01fe76b5d29a533ac76428a7095bfae086
Amiran Koranashvili <amiran.koranashvili@makingscience.com>
2026-02-26 18:10:30 +0400

    made status field final

--- Modulith/src/main/java/com/tskhra/modulith/user_module/exception/HttpException.java
+++ Modulith/src/main/java/com/tskhra/modulith/user_module/exception/HttpException.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public class HttpException extends RuntimeException {
 
-    private HttpStatus status;
+    private final HttpStatus status;
     public HttpException(String message, HttpStatus status) {
         super(message);
         this.status = status;
