5e49d9d9901904e56a6684926decc0abe6b32d8a
Amiran Koranashvili <amiran.koranashvili@makingscience.com>
2026-02-27 18:01:55 +0400

    added login and refresh endpoints

--- Modulith/src/main/java/com/tskhra/modulith/user_module/model/responses/TokensResponse.java
+++ Modulith/src/main/java/com/tskhra/modulith/user_module/model/responses/TokensResponse.java
@@ -1,10 +1,12 @@
 package com.tskhra.modulith.user_module.model.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record TokensResponse(
-        String accessToken,
-        String refreshToken,
-        int expiresIn,
-        int refreshExpiresIn,
-        String tokenType
+        @JsonProperty("access_token") String accessToken,
+        @JsonProperty("refresh_token") String refreshToken,
+        @JsonProperty("expires_in") int expiresIn,
+        @JsonProperty("refresh_expires_in") int refreshExpiresIn,
+        @JsonProperty("token_type") String tokenType
 ) {
 }
