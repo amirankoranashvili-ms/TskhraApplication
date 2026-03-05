6968170aacca1d8216b98ba1fc3694cc3223ede1
Amiran Koranashvili <amiran.koranashvili@makingscience.com>
2026-03-05 16:56:25 +0400

    added city controller

---
+++ Modulith/src/main/java/com/tskhra/modulith/booking_module/controllers/CityController.java
@@ -0,0 +1,26 @@
+package com.tskhra.modulith.booking_module.controllers;
+
+import com.tskhra.modulith.booking_module.services.CityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cities")
+@RequiredArgsConstructor
+public class CityController {
+
+    private final CityService cityService;
+
+    @GetMapping
+    public ResponseEntity<List<String>> getAll() {
+        List<String> cities = cityService.getAllCityNames();
+        return ResponseEntity.status(HttpStatus.OK).body(cities);
+    }
+
+}
--- Modulith/src/main/java/com/tskhra/modulith/booking_module/services/CityService.java
+++ Modulith/src/main/java/com/tskhra/modulith/booking_module/services/CityService.java
@@ -6,7 +6,17 @@ import com.tskhra.modulith.common.exception.HttpNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CityService {
+
+    private final CityRepository cityRepository;
+
+    public List<String> getAllCityNames() {
+        return cityRepository.findAll().stream()
+                .map(City::getName)
+                .toList();
+    }
 }
