33ba0ec1f2c107397ff3572d3fbca60aff0d10f2
Amiran Koranashvili <amiran.koranashvili@makingscience.com>
2026-03-10 03:13:02 +0400

    added query to BookingRepository to filter bookings by business ID, date, and booking statuses

--- Modulith/src/main/java/com/tskhra/modulith/booking_module/repositories/BookingRepository.java
+++ Modulith/src/main/java/com/tskhra/modulith/booking_module/repositories/BookingRepository.java
@@ -1,6 +1,7 @@
 package com.tskhra.modulith.booking_module.repositories;
 
 import com.tskhra.modulith.booking_module.model.domain.Booking;
+import com.tskhra.modulith.booking_module.model.enums.BookingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,4 +16,7 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     @Query("SELECT b FROM Booking b JOIN b.service s WHERE s.business.id = :id AND b.bookingDate = :day")
     List<Booking> getBusinessBookingsByDate(@Param("id") Long id, @Param("day") LocalDate day);
+
+    @Query("SELECT b FROM Booking b JOIN b.service s WHERE s.business.id = :businessId AND b.bookingDate = :date AND b.bookingStatus IN :statuses")
+    List<Booking> findByBusinessIdAndDateAndStatuses(@Param("businessId") Long businessId, @Param("date") LocalDate date, @Param("statuses") List<BookingStatus> statuses);
 }
