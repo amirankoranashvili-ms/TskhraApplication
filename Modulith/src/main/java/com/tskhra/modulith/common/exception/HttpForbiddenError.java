fe59d19fdc6d0fe7ef446760e3983a2866932a57
Amiran Koranashvili <amiran.koranashvili@makingscience.com>
2026-03-09 18:42:06 +0400

    annotated HttpForbiddenError with @NamedInterface to support modularity

--- Modulith/src/main/java/com/tskhra/modulith/common/exception/HttpForbiddenError.java
+++ Modulith/src/main/java/com/tskhra/modulith/common/exception/HttpForbiddenError.java
@@ -1,7 +1,9 @@
 package com.tskhra.modulith.common.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.modulith.NamedInterface;
 
+@NamedInterface
 public class HttpForbiddenError extends HttpException {
     public HttpForbiddenError(String message) {
         super(message, HttpStatus.FORBIDDEN);
