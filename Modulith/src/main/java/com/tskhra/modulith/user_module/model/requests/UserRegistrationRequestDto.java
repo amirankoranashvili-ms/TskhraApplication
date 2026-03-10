05840144599a87ecaee0f65e5ae79dac739ff019
Amiran Koranashvili <amiran.koranashvili@makingscience.com>
2026-03-10 12:00:24 +0400

    added @NotBlank constraint to email field in UserRegistrationRequestDto to enforce non-empty value validation

--- Modulith/src/main/java/com/tskhra/modulith/user_module/model/requests/UserRegistrationRequestDto.java
+++ Modulith/src/main/java/com/tskhra/modulith/user_module/model/requests/UserRegistrationRequestDto.java
@@ -3,11 +3,13 @@ package com.tskhra.modulith.user_module.model.requests;
 import com.tskhra.modulith.user_module.validation.ValidPassword;
 import com.tskhra.modulith.user_module.validation.ValidUsername;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 // todo: validate username != password && email != password
 public record UserRegistrationRequestDto(
         @ValidUsername
         String username,
+        @NotBlank
         @Email
         String email,
         @ValidPassword
