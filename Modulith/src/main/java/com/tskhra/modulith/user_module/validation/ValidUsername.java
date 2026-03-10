5b01ec7a12017bac14bba237d8b01e0b52a48a10
Amiran Koranashvili <amiran.koranashvili@makingscience.com>
2026-03-10 11:58:53 +0400

    added regex pattern to ValidUsername annotation to enforce alphanumeric and underscore constraints

--- Modulith/src/main/java/com/tskhra/modulith/user_module/validation/ValidUsername.java
+++ Modulith/src/main/java/com/tskhra/modulith/user_module/validation/ValidUsername.java
@@ -3,12 +3,14 @@ package com.tskhra.modulith.user_module.validation;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 import java.lang.annotation.*;
 
 @NotBlank(message = "Username cannot be blank")
 @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+@Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Username can only contain letters, numbers, and underscores")
 
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
