78bbd88467d535af34a4ab5e60770171a30f7b3f
Amiran Koranashvili <amiran.koranashvili@makingscience.com>
2026-03-11 17:22:09 +0400

    added indexes in init.sql

--- backups/init.sql
+++ backups/init.sql
@@ -33,22 +33,23 @@ VALUES ('Tbilisi'),
 -- categories
 -- ნაბიჯი 1: ვამატებთ მხოლოდ მთავარ (მშობელ) კატეგორიებს. 
 -- id-ს ბაზა თავისით მიანიჭებს.
-INSERT INTO categories (name) VALUES
-                                ('Legal and Finance'),
-                                ('Beauty and Personal Care'),
-                                ('Health'),
-                                ('Home Services'),
-                                ('Cleaning'),
-                                ('Auto Services'),
-                                ('Education'),
-                                ('Fitness and Sports'),
-                                ('Pets'),
-                                ('Events'),
-                                ('IT and Technology'),
-                                ('Transport');
+INSERT INTO categories (name)
+VALUES ('Legal and Finance'),
+       ('Beauty and Personal Care'),
+       ('Health'),
+       ('Home Services'),
+       ('Cleaning'),
+       ('Auto Services'),
+       ('Education'),
+       ('Fitness and Sports'),
+       ('Pets'),
+       ('Events'),
+       ('IT and Technology'),
+       ('Transport');
 -- ნაბიჯი 2: ვამატებთ ქვეკატეგორიებს.
 -- parent_id ავტომატურად მოიძებნება მთავარი კატეგორიის სახელის მიხედვით.
-INSERT INTO categories (parent_id, name) VALUES
+INSERT INTO categories (parent_id, name)
+VALUES
 -- Legal and Finance
 ((SELECT id FROM categories WHERE name = 'Legal and Finance'), 'Legal Services'),
 ((SELECT id FROM categories WHERE name = 'Legal and Finance'), 'Notary Services'),
@@ -112,3 +113,53 @@ INSERT INTO categories (parent_id, name) VALUES
 ((SELECT id FROM categories WHERE name = 'Transport'), 'Passenger Transportation'),
 ((SELECT id FROM categories WHERE name = 'Transport'), 'Courier and Postal Services'),
 ((SELECT id FROM categories WHERE name = 'Transport'), 'Special Equipment Services');
+
+
+-- INDEXES
+
+-- admins
+CREATE UNIQUE INDEX idx_admins_email ON admins (email);
+CREATE UNIQUE INDEX idx_admins_username ON admins (username);
+-- users
+CREATE UNIQUE INDEX idx_users_keycloak_id ON users (keycloak_id);
+CREATE UNIQUE INDEX idx_users_email ON users (email);
+CREATE INDEX idx_users_user_status ON users (user_status);
+CREATE INDEX idx_users_kyc_status ON users (kyc_status);
+-- categories
+CREATE INDEX idx_categories_parent_id ON categories (parent_id);
+-- cities
+CREATE UNIQUE INDEX idx_cities_name ON cities (name);
+-- user_verification_requests
+CREATE INDEX idx_uvr_user_id ON user_verification_requests (user_id);
+CREATE INDEX idx_uvr_resolved_by ON user_verification_requests (resolved_by);
+-- CREATE INDEX idx_uvr_status ON user_verification_requests (status); FIXME add staus to schema
+-- businesses
+CREATE INDEX idx_businesses_user_id ON businesses (user_id);
+CREATE INDEX idx_businesses_category_id ON businesses (category_id);
+CREATE INDEX idx_businesses_address_id ON businesses (address_id);
+CREATE INDEX idx_businesses_activity_status ON businesses (activity_status);
+-- addresses
+CREATE INDEX idx_addresses_city_id ON addresses (city_id);
+-- resources
+CREATE INDEX idx_resources_business_id ON resources (business_id);
+CREATE INDEX idx_resources_activity_status ON resources (activity_status);
+-- services
+CREATE INDEX idx_services_business_id ON services (business_id);
+CREATE INDEX idx_services_activity_status ON services (activity_status);
+-- service_resources
+CREATE INDEX idx_service_resources_resource_id ON service_resources (resource_id);
+-- business_schedules
+CREATE INDEX idx_bs_business_id ON business_schedules (business_id);
+-- business_unavailable_schedules
+CREATE INDEX idx_bus_business_id ON business_unavailable_schedules (business_id);
+-- business_unavailable_onetimes
+CREATE INDEX idx_buo_business_id ON business_unavailable_onetimes (business_id);
+CREATE INDEX idx_buo_date ON business_unavailable_onetimes (date);
+-- bookings
+CREATE INDEX idx_bookings_user_id ON bookings (user_id);
+CREATE INDEX idx_bookings_service_id ON bookings (service_id);
+CREATE INDEX idx_bookings_resource_id ON bookings (resource_id);
+CREATE INDEX idx_bookings_booking_date ON bookings (booking_date);
+CREATE INDEX idx_bookings_booking_status ON bookings (booking_status);
+CREATE INDEX idx_bookings_booked_at ON bookings (booked_at);
+CREATE INDEX idx_bookings_status_date ON bookings (booking_status, booking_date);
