ea694f763405f3f64655b223d5354f4787b31b8d
Amiran Koranashvili <amiran.koranashvili@makingscience.com>
2026-03-23 13:28:51 +0400

    Added MyCustomStatus enum with status resolution logic

---
+++ Modulith/src/main/java/com/tskhra/modulith/common/exception/custom_status_exceptions/CustomStatusException.java
@@ -0,0 +1,21 @@
+package com.tskhra.modulith.common.exception.custom_status_exceptions;
+
+import com.tskhra.modulith.common.model.enums.MyCustomStatus;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.modulith.NamedInterface;
+
+@Getter
+@NamedInterface
+public class CustomStatusException extends RuntimeException {
+
+    private final int customStatusCode;
+    private final HttpStatus httpStatus;
+
+    public CustomStatusException(MyCustomStatus myCustomStatus) {
+        super(myCustomStatus.description());
+        this.customStatusCode = myCustomStatus.value();
+        this.httpStatus = myCustomStatus.httpStatus();
+    }
+
+}
