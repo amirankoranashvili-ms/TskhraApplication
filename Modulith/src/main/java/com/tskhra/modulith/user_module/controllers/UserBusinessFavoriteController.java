adfa906fd8f7da61032e8c10446a76bc99171e79
Amiran Koranashvili <amiran.koranashvili@makingscience.com>
2026-03-25 12:36:47 +0400

    Feature: Favorite functionality added

--- Modulith/src/main/java/com/tskhra/modulith/user_module/controllers/UserBusinessFavoriteController.java
+++ Modulith/src/main/java/com/tskhra/modulith/user_module/controllers/UserBusinessFavoriteController.java
@@ -5,12 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/users/me/favorites")
+@RestController
+@RequestMapping("/users/me/favorites")
 @RequiredArgsConstructor
 public class UserBusinessFavoriteController {
 
