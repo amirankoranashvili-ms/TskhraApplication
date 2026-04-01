cbe33f75bee47faeea26c88bf85ffeb4e2d7b032
Amiran Koranashvili <amiran.koranashvili@makingscience.com>
2026-04-01 16:54:26 +0400

    added logging to GreetingController

--- Modulith/src/main/java/com/tskhra/modulith/messaging_module/controllers/GreetingController.java
+++ Modulith/src/main/java/com/tskhra/modulith/messaging_module/controllers/GreetingController.java
@@ -1,16 +1,19 @@
 package com.tskhra.modulith.messaging_module.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@Slf4j
 @Controller
 public class GreetingController {
 
     @MessageMapping("/greeting")
     public String handle(String greeting) {
+        log.info("Received message: {}", greeting);
         return "[" + getTimestamp() + ": " + greeting;
     }
 
